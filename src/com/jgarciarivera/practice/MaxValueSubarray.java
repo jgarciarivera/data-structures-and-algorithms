1cdbf224a28ac1d567a68059e22ecd25a85e9f5a
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2023-01-19 15:13:56 -0600

    Update MaxValueSubarray method to allow for dynamic size subarray.

--- src/com/jgarciarivera/practice/MaxValueSubarray.java
+++ src/com/jgarciarivera/practice/MaxValueSubarray.java
@@ -1,23 +1,24 @@
 package com.jgarciarivera.practice;
 
-// Determines the max value of a subarray size 3
+// Determines the max value of a subarray of size k in an array size n
 public class MaxValueSubarray {
 
     public static void main(String[] args) {
-        int[] array = {4, 2, 1, 7, 8, 1, 2, 8, 1, 0};
-        int max = maxValue(array);
-        System.out.println("Max value: " + max);
+        int result = findMaxValue(new int[] {4, 2, 1, 7, 8, 1, 2, 8, 1, 0}, 3);
+        System.out.println("Max value: " + result);
     }
-    public static int maxValue(int[] array) {
-        int maxSoFar = Integer.MIN_VALUE;
-
-        for (int i = 0; i < array.length - 2; i++) {
-            int currentSubarraySum = array[i] + array[i + 1] + array[i + 2];
-            if (currentSubarraySum > maxSoFar) {
-                maxSoFar = currentSubarraySum;
+    public static int findMaxValue(int[] array, int subarraySize) {
+        int maxSum  = Integer.MIN_VALUE;
+        int runningSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            runningSum = runningSum + array[i];
+            if (i >= subarraySize - 1) {
+                if (runningSum > maxSum) {
+                    maxSum = runningSum;
+                }
+                runningSum = runningSum - array[i - (subarraySize - 1)];
             }
         }
-
-        return maxSoFar;
+        return maxSum;
     }
 }
