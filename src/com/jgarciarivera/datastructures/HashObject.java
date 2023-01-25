9a8027cfa8194e7164c9acc27e487e6729b4026b
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2023-01-24 19:58:46 -0600

    Add WIP HashTable implementation.

---
+++ src/com/jgarciarivera/datastructures/HashObject.java
@@ -0,0 +1,11 @@
+package com.jgarciarivera.datastructures;
+
+public class HashObject {
+    Object key;
+    Object value;
+
+    public HashObject(Object key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+}
